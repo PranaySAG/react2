00cd2eb6b9f77093e57a1693da4ded8325687773
Harshofft <hdharsh122@gmail.com>
2024-08-23 16:41:18 +0530

    completed finally i learn something

---
+++ src/component/footer/footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './footer.css'
+function footer() {
+  return (
+        <div className="footer">
+            <h1>Footer</h1>
+            <h2>Email:hdharsh@gmail.com</h2>
+            </div>
+    )
+}
+
+
+export default footer
---
+++ src/component/nav-bar/navbar.js
@@ -0,0 +1,18 @@
+ import React from 'react'
+ import './nav.css'
+ import { Link } from 'react-router-dom'
+ 
+ function Navbar() {
+   return (
+     <div className='nav-container'>
+        <h1 className='logo'>Foodies</h1>
+        <div className='nav-item'>
+        <Link to='/' className='links' >Home</Link>
+        <Link to='/about' className='links' >About</Link>
+        <Link to='/contact' className='links' >Contact</Link>
+        </div>
+     </div>
+   )
+ }
+ 
+ export default Navbar
